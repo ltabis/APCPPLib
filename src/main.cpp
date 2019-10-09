6376569e6b39751aaf356063f84d28850c28e0e4
Lucas <lucas.tabis@epitech.eu>
2019-10-09 23:40:10 +0200

    [fix] compilation change : Using a CMakeList to compile, deleted Makefiles. Added a basic implementation of an Exception object.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
+#include <vector>
+#include "Exception.hpp"
+
+void func()
+{
+    throw Log::Exception("Malloc buffer invalid", "func");
+}
+
 int main()
 {
+    try {
+        func();
+    } catch (Log::Exception &except) {
+        except.debugErrorMessage();
+    }
 	return 0;
 }
