5fc068500a7c1aba5dde3ed20364663dffe094c9
Lucas <lucas.tabis@epitech.eu>
2019-10-29 21:42:04 +0100

    [minor] example : Added example of breakpoints and log usage.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
-#include <chrono>
+#include <iostream>
 #include "Logger.hpp"
+#include "Breakpoint.hpp"
 
 int main()
 {
-    using namespace std::chrono_literals;
+	using namespace std::chrono_literals;
 
-    Debug::Logger *log = Debug::Logger::getInstance(Debug::STANDARD, Debug::Flags::debug_off | Debug::Flags::fatal_off);
-    std::string a = "A default debug message.";
-
-    log->generateDebugMessage(Debug::DEBUG, "Just a regular debug message", "main");
-    log->generateDebugMessage(Debug::INFO, "Info message", "main");
-    log->generateDebugMessage(Debug::WARNING, "Warning message, attention !", "main");
-    log->generateDebugMessage(Debug::ERROR, "Error", "main");
-    log->generateDebugMessage(Debug::FATAL, "Fatal message, everything is going haywire !", "main");
-    std::this_thread::sleep_for(2s);
-    return 0;
+	Debug::Logger *log = Debug::Logger::getInstance(Debug::STANDARD, Debug::Flags::debug_off | Debug::Flags::fatal_off);
+	{
+		Debug::Breakpoint b("Logger class", "main", Debug::Flags::all_on);
+		log->generateDebugMessage(Debug::DEBUG, "Just a regular debug message", "main");
+		log->generateDebugMessage(Debug::INFO, "Info message", "main");
+		log->generateDebugMessage(Debug::WARNING, "Warning message, attention !", "main");
+		log->generateDebugMessage(Debug::ERROR, "Error", "main");
+		log->generateDebugMessage(Debug::FATAL, "Fatal message, everything is going haywire !", "main");
+	}
+	Debug::Breakpoint b1("unused breakpoint", "main", Debug::Flags::debug_off);
+	Debug::Breakpoint b2("Sleep", "main", Debug::Flags::all_on);
+	std::this_thread::sleep_for(2s);
+	return 0;
 }
