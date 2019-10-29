b90f93b43002f82418556bfab36dda9c939ebb87
Lucas <lucas.tabis@epitech.eu>
2019-10-29 21:35:19 +0100

    [feat] Benchmarking : Added first benchmarking tools : Breakpoint, wich mesure the time at instantiation and destruction.

--- include/macro.hpp
+++ include/macro.hpp
@@ -12,3 +12,18 @@
 #define MAGENTA         "\033[0;35m"
 #define CYAN            "\033[0;36m"
 #define WHITE           "\033[m"
+
+#define DEFAULT_LOG_FILE    "output.txt"
+#define WARNING_MESSAGE     "[Warning]"
+#define ERROR_MESSAGE       "[Error]"
+#define DEBUG_MESSAGE       "[Debug]"
+#define FATAL_MESSAGE       "[FATAL]"
+#define INFO_MESSAGE        "[Info]"
+#define BREAKPOINT_MESSAGE  "[" REDUNDERLINED "0" WHITE "][Breakpoint]"
+
+// Flags options for the Logger class
+namespace Debug {
+    namespace Flags {
+        typedef enum e_options {all_on = 1, debug_off = 2, info_off = 4, warning_off = 8, error_off = 16, fatal_off = 32} options;
+    }
+}
