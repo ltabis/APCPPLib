ac1db134cff496bab2eac285bd01884cff2c7167
Lucas <lucas.tabis@epitech.eu>
2019-11-02 16:24:10 +0100

    [feat] unit tests : Added some more unit tests.

--- tests/Exception/test_exception.cpp
+++ tests/Exception/test_exception.cpp
@@ -19,8 +19,16 @@ TEST(ExceptionTests, normalThrow)
     ASSERT_STREQ("\x1B[0;31m[Exception] \x1B[0;35mAn error occurred\x1B[m in method \x1B[0;34mSomewhere\n", output.c_str());
 }
 
-int main(int argc, char **argv)
+TEST(ExceptionTests, stringThrow)
 {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    testing::internal::CaptureStderr();
+    std::string output;
+
+    try {
+        throw Debug::Exception(std::string("An error occurred"), std::string("Somewhere"));
+    } catch (const Debug::Exception &exception) {
+        exception.debugErrorMessage();
+    }
+    output = testing::internal::GetCapturedStderr();
+    ASSERT_STREQ("\x1B[0;31m[Exception] \x1B[0;35mAn error occurred\x1B[m in method \x1B[0;34mSomewhere\n", output.c_str());
 }
