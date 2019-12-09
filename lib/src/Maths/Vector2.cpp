ffa2e2980f820d8e2906c4acd5ae04f5aa683b30
Lucas <lucas.tabis@epitech.eu>
2019-12-09 13:17:12 +0100

    [:bug: bug fix] Deleted 3Dimentional fields for vector2.

--- lib/src/Maths/Vector2.cpp
+++ lib/src/Maths/Vector2.cpp
@@ -4,8 +4,8 @@
 
 #include "Vector2.hpp"
 
-template<typename T> Maths::Vector2<T>::Vector2() : _x(0), _y(0), _z(0) {}
+template<typename T> Maths::Vector2<T>::Vector2() : _x(0), _y(0) {}
 
-template<typename T> Maths::Vector2<T>::Vector2(T value) : _x(value), _y(value), _z(value) {}
+template<typename T> Maths::Vector2<T>::Vector2(T value) : _x(value), _y(value) {}
 
-template<typename T> Maths::Vector2<T>::Vector2(T x, T y) : _x(x), _y(y), _z(z) {}
+template<typename T> Maths::Vector2<T>::Vector2(T x, T y) : _x(x), _y(y) {}
