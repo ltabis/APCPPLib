02ffccd181eb6388d929b4539a16a70b39bd2727
Lucas <lucas.tabis@epitech.eu>
2019-12-09 14:21:17 +0100

    [:sparkles: new feature] Vector3 implementation.

---
+++ lib/src/Maths/Vector3.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tabis on 09/10/2019.
+//
+
+#include "Vector3.hpp"
--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
 #include "Logger.hpp"
 #include "Breakpoint.hpp"
 #include "Vector2.hpp"
+#include "Vector3.hpp"
 
 int main()
 {
-	Maths::Vector2<float> vector(9.5f, 105.2f);
+	Maths::Vector2<float> vector2(9.5f, 105.2f);
+	Maths::Vector3<int> vector3(9, 105, 10);
 
-	std::cout << vector << std::endl;
+	std::cout << vector2 << std::endl;
+	std::cout << vector3 << std::endl;
 	return 0;
 }
