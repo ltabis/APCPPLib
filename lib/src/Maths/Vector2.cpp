785cd7b53f2559e08ed20f737ef72d5785986d12
Lucas <lucas.tabis@epitech.eu>
2019-12-09 13:38:30 +0100

    [:bug: bug fix] Moved everything in the .hpp file.

--- lib/src/Maths/Vector2.cpp
+++ lib/src/Maths/Vector2.cpp
@@ -3,29 +3,3 @@
 //
 
 #include "Vector2.hpp"
-
-template<typename T> Maths::Vector2<T>::Vector2() : _x(0), _y(0) {}
-
-template<typename T> Maths::Vector2<T>::Vector2(T value) : _x(value), _y(value) {}
-
-template<typename T> Maths::Vector2<T>::Vector2(T x, T y) : _x(x), _y(y) {}
-
-template<typename T> T Maths::Vector2<T>::x() const
-{
-    return _x;
-}
-
-template<typename T> T Maths::Vector2<T>::y() const
-{
-    return _y;
-}
-
-template<typename T> void Maths::Vector2<T>::setX(T value)
-{
-    _x = value;
-}
-
-template<typename T> void Maths::Vector2<T>::setY(T value)
-{
-    _y = value;
-}
--- lib/src/Maths/Vector2.hpp
+++ lib/src/Maths/Vector2.hpp
@@ -14,33 +14,45 @@ namespace Maths {
     public:
         /// \brief Ctor
         /// Construct the Vector2 class
-        Vector2();
+        Vector2() : _x(0), _y(0) {}
         /// \brief Ctor
         /// Construct the Vector2 class
         /// \param value : value of all fields
-        Vector2(T value);
+        Vector2(T value) : _x(value), _y(value) {}
         /// \brief Ctor
         /// Construct the Vector2 class
         /// \param x : x field value
         /// \param y : y field value
-        Vector2(T x, T y);
+        Vector2(T x, T y) : _x(x), _y(y) {}
         /// \brief Dtor
         /// Destroy the Vector2 class
         ~Vector2() = default;
 
         /// \brief X getter
         /// \return the value of the x field.
-        T x() const;
+        T x() const
+        {
+            return _x;
+        }
         /// \brief Y getter
         /// \return the value of the y field.
-        T y() const;
+        T y() const
+        {
+            return _y;
+        }
 
         /// \brief X setter
         /// \param value : the new value of the x field.
-        void setX(T value);
+        void setX(T value)
+        {
+            _x = value;
+        }
         /// \brief Y setter
         /// \param value : the new value of the y field.
-        void setY(T value);
+        void setY(T value)
+        {
+            _y = value;
+        }
 
     private:
         T _x, _y;
