98fb5dc44ac3eb65862e2356584fb8487f9ab9a1
Lucas <lucas.tabis@epitech.eu>
2019-12-27 00:44:42 +0100

    :white_check_mark: Added unit tests for the tuple max value method.

---
+++ tests/Utils/test_tuples.cpp
@@ -0,0 +1,22 @@
+//
+// Created by tabis on 10/10/2019.
+//
+
+#include "Tuples.hpp"
+#include <gtest/gtest.h>
+
+TEST(TuplesTests, One)
+{
+    ASSERT_EQ(Utils::max_tuple_value(std::make_tuple(1)), 1);
+}
+
+TEST(TuplesTests, MutipleInt)
+{
+    ASSERT_EQ(Utils::max_tuple_value(std::make_tuple(150, 200, 7, 89)), 200);
+}
+
+TEST(TuplesTests, MutipleFloats)
+{
+    ASSERT_EQ(Utils::max_tuple_value(std::make_tuple(55.0f, 21.0f, 1.0f, 360.5f)), 360.5f);
+    ASSERT_EQ(Utils::max_tuple_value(std::make_tuple(5.32f, 21.513f, 21.512f)), 21.513f);
+}
