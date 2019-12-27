cafa8af6a3f44b28f3d1877f3f7ca6fdb3af191b
Lucas <lucas.tabis@epitech.eu>
2019-12-27 01:04:28 +0100

    :sparkles: Added a compile time function that calculates the sum of a tuple (+ tests).

--- lib/src/Utils/Tuples.hpp
+++ lib/src/Utils/Tuples.hpp
@@ -12,7 +12,9 @@
 namespace Utils
 {
 
-    /// \brief A compile time recursive function that calculate the maximum value of a tuple 
+    /// \brief A compile time recursive function that calculate the maximum value of a tuple.
+    /// \param tuple : the tuple that we want to iterate through.
+    /// \return A type specified by the variadic template.
     template<size_t I = 0, typename ... T>
     auto max_tuple_value(const std::tuple<T ...> &tuple)
     {
@@ -24,6 +26,20 @@ namespace Utils
 
             return current > next ? current : next;
         }
+
+        // Where at the end of the tuple, returning the last element.
+        return std::get<I>(tuple);
+    }
+
+    /// \brief A compile time recursive function that calculate the sum elements of a tuple.
+    /// \param tuple : the tuple that we want to iterate through.
+    /// \return A type specified by the variadic template.
+    template<size_t I = 0, typename ... T>
+    auto tuple_sum(const std::tuple<T ...> &tuple)
+    {
+        // Adding the current value to the next.
+        if constexpr(I + 1 != sizeof ...(T))
+            return std::get<I>(tuple) + tuple_sum<I + 1>(tuple);
 
         // Where at the end of the tuple, returning the last element.
         return std::get<I>(tuple);
--- tests/Utils/test_tuples.cpp
+++ tests/Utils/test_tuples.cpp
@@ -20,3 +20,13 @@ TEST(TuplesTests, MutipleFloats)
     ASSERT_EQ(Utils::max_tuple_value(std::make_tuple(55.0f, 21.0f, 1.0f, 360.5f)), 360.5f);
     ASSERT_EQ(Utils::max_tuple_value(std::make_tuple(5.32f, 21.513f, 21.512f)), 21.513f);
 }
+
+TEST(TuplesTests, addMutipleInt)
+{
+    ASSERT_EQ(Utils::tuple_sum(std::make_tuple(104, 2, 10, 1000)), 1116);
+}
+
+TEST(TuplesTests, addMutipleFloats)
+{
+    ASSERT_EQ(Utils::tuple_sum(std::make_tuple(55.0f, 21.0f, 1.0f, 360.5f)), 437.5f);
+}
