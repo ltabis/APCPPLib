c996d2aae137c0feb822dd181bff2d3994fd17e2
Lucas <lucas.tabis@epitech.eu>
2019-12-28 11:43:20 +0100

    :white_check_mark: Added unit lvalue unit tests for tuples.

--- tests/Utils/test_tuples.cpp
+++ tests/Utils/test_tuples.cpp
@@ -30,3 +30,17 @@ TEST(TuplesTests, addMutipleFloats)
 {
     ASSERT_EQ(Utils::tuple_sum(std::make_tuple(55.0f, 21.0f, 1.0f, 360.5f)), 437.5f);
 }
+
+TEST(TuplesTests, sumLvalue)
+{
+    std::tuple<int, int, int, int, int, int> tpl(1, 10, 100, 1000, 10000, 100000);
+ 
+    ASSERT_EQ(Utils::tuple_sum(tpl), 111111);
+}
+
+TEST(TuplesTests, maxLvalue)
+{
+    std::tuple<int, int, int, int, int, int> tpl(1, 10, 100, 1000, 10000, 100000);
+ 
+    ASSERT_EQ(Utils::max_tuple_value(tpl), 100000);
+}
