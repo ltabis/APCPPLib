c966112bb6a67bd46e5519b630c340bcfa3360b2
Lucas <lucas.tabis@epitech.eu>
2020-01-21 22:40:39 +0100

    :arrow_up: Updated scene manager system by adding methods for the SceneStateManager object + added update() and onCreate() methods for the IScene interface.

--- lib/src/Game/IScene.hpp
+++ lib/src/Game/IScene.hpp
@@ -9,19 +9,27 @@
 
 #include <stack>
 #include <queue>
+#include <string>
 #include <cstring>
 
 /// \namespace Scenes
 /// \brief Used for all scenes components
-namespace Scenes {
+namespace Game
+{
 
     /// \class IScene
     /// \brief Scene interface
-    class IScene {
+    class IScene
+    {
         public:
-            /// \brief destructor
-            /// Destroy IScene class
-            virtual ~IScene() = default;
+
+            /// \return a scene representing the state of the scene
+            /// \brief run the scenes logic
+            virtual void onCreate() = 0;
+
+            /// \return a scene representing the state of the scene
+            /// \brief run the scenes logic
+            virtual void update() = 0;
 
             /// \param name : the new name of the current scene
             /// \brief change the name of the scene
@@ -31,33 +39,11 @@ namespace Scenes {
             /// \brief get the name of the current scene
             virtual std::string name() const = 0;
 
-            /// \return a scene representing the state of the scene
-            /// \brief run the scenes logic
-            virtual IScene *run() = 0;
-
-            /// \return a bool if the scene needs to be poped when destroyed
-            /// \brief scene deletion characteristic
-            virtual bool isToPop() const = 0;
-
-            /// \return a bool if the scene needs to be swaped when destroyed
-            /// \brief scene deletion characteristic
-            virtual bool isToSwap() const = 0;
-
             /// \param bool : the visible state of the scene
             /// \brief set visible state of the scene
             virtual void setVisible(bool b) = 0;
-
-            /// \param bool : if the scene as to be poped
-            /// \brief set the pop state of the scene
-            virtual void setPop(bool pop) = 0;
             
             /// \brief remove all the entities of the scene
             virtual void remove() = 0;
-
-            /// \param event : stack of all event
-            /// Int is the number of the player
-            /// size_t is the event
-            /// \brief manage all event in the scene
-            virtual void handleEvent(std::queue<std::pair<int, size_t>> event) = 0;
     };
 }
--- lib/src/Game/SceneStateMachine.cpp
+++ lib/src/Game/SceneStateMachine.cpp
@@ -3,3 +3,56 @@
 //
 
 #include "SceneStateMachine.hpp"
+
+void Game::SceneStateMachine::push(std::shared_ptr<Game::IScene> &&scene)
+{
+    _scenes.push(scene);
+}
+
+void Game::SceneStateMachine::pop()
+{
+    _scenes.pop();
+}
+
+void Game::SceneStateMachine::pop(const std::string &name)
+{
+    while (_scenes.top()->name() != name)
+        _scenes.pop();
+}
+
+void Game::SceneStateMachine::swap(std::shared_ptr<Game::IScene> &&scene)
+{
+    pop();
+    _scenes.push(scene);
+}
+
+std::size_t Game::SceneStateMachine::size() const
+{
+    return _scenes.size();
+}
+
+bool Game::SceneStateMachine::empty() const
+{
+    return _scenes.empty();
+}
+
+std::string Game::SceneStateMachine::name() const
+{
+    return _scenes.top()->name();
+}
+
+bool Game::SceneStateMachine::update()
+{
+    return _scenes.top()->update();
+}
+
+void Game::SceneStateMachine::clear()
+{
+    while (!empty())
+        pop();
+}
+
+void Game::SceneStateMachine::remove()
+{
+    _scenes.top()->remove();
+}
