76febff8693b0167916434a6d55d2818260717fc
Lucas <lucas.tabis@epitech.eu>
2020-01-22 14:09:06 +0100

    :white_check_mark: Added some more unit tests for advances methods of the scene state machine object.

--- tests/Game/test_sceneStateMachine.cpp
+++ tests/Game/test_sceneStateMachine.cpp
@@ -35,7 +35,7 @@ public:
 		}
 };
 
-TEST(SceneStateMachineTests, SceneMachineBasicMethods)
+TEST(SceneStateMachineTests, SceneStateMachine_basic_methods)
 {
     // Creating a new scene state machine, casting it into the IMediator interface.
 	std::shared_ptr<Game::SceneStateMachine> machine = std::make_shared<Game::SceneStateMachine>(Game::SceneStateMachine());
@@ -52,11 +52,88 @@ TEST(SceneStateMachineTests, SceneMachineBasicMethods)
 
     // The manager has one scene in memory, updating it.
     ASSERT_EQ(machine->size(), 1);
-	ASSERT_EQ(machine->update(), false);
+	ASSERT_EQ(machine->update(), true);
 
     // Clearing the machine.
 	machine->clear();
 
     // Has the machine been cleared ?
+    ASSERT_EQ(machine->size(), 0);
+}
+
+TEST(SceneStateMachineTests, SceneStateMachine_scene_manipulation)
+{
+    // Creating a new scene state machine, casting it into the IMediator interface.
+	std::shared_ptr<Game::SceneStateMachine> machine = std::make_shared<Game::SceneStateMachine>(Game::SceneStateMachine());
+	std::shared_ptr<Game::IMediator> ptr(machine);
+
+    // The interface is used to notify the scene manager that a machine stoped running.
+	std::shared_ptr<Game::IScene> scene1(new SampleScene("SampleScene1", ptr));
+	std::shared_ptr<Game::IScene> scene2(new SampleScene("SampleScene2", ptr));
+
+    // No scenes to update, return false.
+	ASSERT_EQ(machine->update(), false);
+
+    // Adding scenes to the machine.
+	machine->push(scene1);
+	machine->push(scene2);
+
+    // The manager has two scenes in memory, updating the first one.
+    ASSERT_EQ(machine->size(), 2);
+	ASSERT_EQ(machine->update(), true);
+
+	// Checking the name of the scene on the top of the stack.
+    ASSERT_EQ(machine->name(), "SampleScene2");
+
+	// Poping the second scene.
+	machine->pop();
+    ASSERT_EQ(machine->name(), "SampleScene1");
+    ASSERT_EQ(machine->size(), 1);
+
+    // Clearing the machine.
+	machine->clear();
+
+    // Has the machine been cleared ?
+    ASSERT_EQ(machine->size(), 0);
+}
+
+TEST(SceneStateMachineTests, SceneStateMachine_advanced_scene_manipulation)
+{
+    // Creating a new scene state machine, casting it into the IMediator interface.
+	std::shared_ptr<Game::SceneStateMachine> machine = std::make_shared<Game::SceneStateMachine>(Game::SceneStateMachine());
+	std::shared_ptr<Game::IMediator> ptr(machine);
+
+    // The interface is used to notify the scene manager that a machine stoped running.
+	std::shared_ptr<Game::IScene> scene1(new SampleScene("SampleScene1", ptr));
+	std::shared_ptr<Game::IScene> scene2(new SampleScene("SampleScene2", ptr));
+
+    // No scenes to update, return false.
+	ASSERT_EQ(machine->update(), false);
+
+    // Adding scenes to the machine.
+	machine->push(scene1);
+
+    // The manager has one scene in memory, updating it.
+    ASSERT_EQ(machine->size(), 1);
+	ASSERT_EQ(machine->update(), true);
+
+	// Checking the name of the scene on the top of the stack.
+    ASSERT_EQ(machine->name(), "SampleScene1");
+
+	// Swaping.
+	machine->swap(scene2);
+    ASSERT_EQ(machine->name(), "SampleScene2");
+    ASSERT_EQ(machine->size(), 1);
+
+	// Removing elements from the current scene.
+	machine->remove();
+
+	// Push the first one again.
+	machine->push(scene1);
+
+	// Poping via string.
+	machine->pop("unknown");
+
+	// There isn't any scenes that are called "unknown" so everything as been poped.
     ASSERT_EQ(machine->size(), 0);
 }
