c0f92d394791f089369517d29551f17cc82041a3
Lucas <lucas.tabis@epitech.eu>
2020-02-11 19:38:51 +0100

    :white_check_mark: Added a basic mock test for the bubble sort algortihm + updated Cmakelist.

--- tests/Algorithm/test_bubbleSort.cpp
+++ tests/Algorithm/test_bubbleSort.cpp
@@ -3,10 +3,14 @@
 //
 
 #include "BubbleSort.hpp"
-#include <gtest/gtest.h>
+#include "gmock/gmock.h"
 
 TEST(BubbleSortTests, bubble_sort)
 {
     std::vector<int> vec{1, 0, 32, 4, 4, 5, 2};
-    ASSERT_THAT(Algorithm::bubble_sort(vec));
+
+    // Bubble sorting the vector.
+    Algorithm::bubble_sort(vec);
+
+    ASSERT_THAT(vec, testing::ElementsAre(0, 1, 2, 4, 4, 5, 32));
 }
