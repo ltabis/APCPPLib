f86ba09269766457c57308ce0a36f2baa82027a8
Lucas <lucas.tabis@epitech.eu>
2020-02-14 17:21:31 +0100

    :pencil: Added documentation for the AScene and IScene headers.

--- lib/src/Game/AScene.hpp
+++ lib/src/Game/AScene.hpp
@@ -1,26 +1,53 @@
+/**
+ *  @file     lib/src/Game/AScene.hpp
+ *  @author   tabis on the 2020-02-14
+ *  @date     2020-02-14
+ * 
+ *  project: APCPPLib
+ * 
+ */
 
 #pragma once
 
 #include "IScene.hpp"
 #include "SceneStateMachine.hpp"
 
+/// @namespace Game.
+/// @brief game namespace.
 namespace Game
 {
 
+    /// @enum AScene
+    /// @brief An abstract class used to implement redundant methods of the IScene interface.
     class AScene : public IScene
     {
 
         public:
+            /** 
+             * @brief Constructor.
+             * @param name : the name of the scene.
+             * @param mediator : the mediator instance needed to call the `notify` method.
+             */
             AScene(const std::string &name, std::shared_ptr<Game::IMediator> &mediator);
 
+            /** 
+             * @brief Set the name of the scene.
+             * @param name : the new name of the scene.
+             */
             void setName(const std::string &name) override;
 
+            /** 
+             * @brief Get the name of the scene.
+             * @return the name of the scene.
+             */
             std::string name() const override;
 
         protected:
 
+            /*! The name of the scene */
             std::string _name;
 
+            /*! The mediator interface instance */
             std::shared_ptr<Game::IMediator> _mediator;
     };
 }
--- lib/src/Game/IScene.hpp
+++ lib/src/Game/IScene.hpp
@@ -1,9 +1,11 @@
-/*
-** EPITECH PROJECT, 2019
-** IScene.hpp
-** File description:
-** IScene class interface
-*/
+/**
+ *  @file     lib/src/Game/IScene.hpp
+ *  @author   tabis on the 2020-02-14
+ *  @date     2020-02-14
+ * 
+ *  project: APCPPLib
+ * 
+ */
 
 #pragma once
 
@@ -12,29 +14,53 @@
 #include <string>
 #include <cstring>
 
-/// \namespace Scenes
-/// \brief Used for all scenes components
+/// @namespace Scenes
+/// @brief Used for all scenes components
 namespace Game
 {
 
-    /// \class IScene
-    /// \brief Scene interface
+    /// @class IScene
+    /// @brief Scene interface
     class IScene
     {
         public:
 
+            /** 
+             * @brief Called just after the scene is created.
+             */
             virtual void onCreate() = 0;
 
+            /** 
+             * @brief Called just before the destructor is called.
+             */
             virtual void onDestroy() = 0;
 
+            /** 
+             * @brief Used to activate elements of the scene when switching to it.
+             */
             virtual void onActivate() = 0;
 
+            /** 
+             * @brief Used to set inavtive elements of the scene when we push an other scene to the stack.
+             */
             virtual void onDeactivate() = 0;
 
+            /** 
+             * @brief Update the scene.
+             * @param deltaTime : time computed since the last frame.
+             */
             virtual void onUpdate(float deltaTime) = 0;
 
+            /** 
+             * @brief Set the name of the scene.
+             * @param name : the new name of the scene. 
+             */
             virtual void setName(const std::string &name) = 0;
 
+            /** 
+             * @brief get the name of the scene.
+             * @return the name of the scene.
+             */
             virtual std::string name() const = 0;
     };
 }
